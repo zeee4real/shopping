821fd7cf9f82ff8856f5ad5c3935400d0d08319a
zeee4real <asadulla.zaid@gmail.com>
2021-02-26 18:59:50 +0530

    first commit

---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping/providers/product.dart';
+import 'package:shopping/providers/products.dart';
+
+import '../providers/products.dart';
+import '../providers/product.dart';
+import '../widgets/product_item.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, index) => ChangeNotifierProvider(
+        create: (ctx) => products[index],
+        child: ProductItem(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
+
+class ProductItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () {
+              product.toggleFavorite();
+            },
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
