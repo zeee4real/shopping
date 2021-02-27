8f8fed147659b8eeba83d146136a3bf88c8999e9
zeee4real <asadulla.zaid@gmail.com>
2021-02-27 19:35:37 +0530

    - added cart screen list - dismissable items

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -46,7 +47,19 @@ class CartScreen extends StatelessWidget {
                 ],
               ),
             ),
-          )
+          ),
+          SizedBox(height: 10),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: (_, i) => CartItem(
+              id: cart.items.values.toList()[i].id,
+              productId: cart.items.keys.toList()[i],
+              title: cart.items.values.toList()[i].title,
+              quantity: cart.items.values.toList()[i].quantity,
+              price: cart.items.values.toList()[i].price,
+            ),
+            itemCount: cart.items.length,
+          ))
         ],
       ),
     );
